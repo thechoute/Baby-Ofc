c4023464f87b6606c9bce6f541937dd4369c99a4
thechoute <82709682+thechoute@users.noreply.github.com>
2021-12-21 18:54:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,7 @@ apt-get install ffmpeg libwebp -y
 apt-get install wget -y
 apt-get install imagemagick -y
 apt-get install tesseract -y
-npm i node-tesseract-ocr
-npm i @adiwajshing/baileys@3.4.1
-pkg install termimage
-pkg install toilet
-pkg install cowsay
-pkg install neofetch
+npm install
 
 
 echo -e '\e[1;32m

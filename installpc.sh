86cac94a367cd2e4c8be35d8815dd9d54450d015
thechoute <82709682+thechoute@users.noreply.github.com>
2021-12-21 12:51:00 -0400

    Create installpc.sh

---
+++ installpc.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+echo -e '\e[1;32m
+▀█▀ █▀▀▄ █▀▀ ▀▀█▀▀ █▀▀█ █── █▀▀█ █▀▀▄ █▀▀▄ █▀▀█ 　 ░█▀▄▀█ █▀▀█ █▀▀▄ █──█ █── █▀▀█ █▀▀ 
+░█─ █──█ ▀▀█ ──█── █▄▄█ █── █▄▄█ █──█ █──█ █──█ 　 ░█░█░█ █──█ █──█ █──█ █── █──█ ▀▀█ 
+▄█▄ ▀──▀ ▀▀▀ ──▀── ▀──▀ ▀▀▀ ▀──▀ ▀──▀ ▀▀▀─ ▀▀▀▀ 　 ░█──░█ ▀▀▀▀ ▀▀▀─ ─▀▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀ 
+░█▄─░█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀█ █▀▀█ ─▀─ █▀▀█ █▀▀ 
+░█░█░█ █▀▀ █── █▀▀ ▀▀█ █▄▄█ █▄▄▀ ▀█▀ █──█ ▀▀█ 
+░█──▀█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀──▀ ▀─▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀'
+
+npm i node-tesseract-ocr
+npm i @adiwajshing/baileys@3.4.1
+npm install
+npm i ffmpeg
+npm i cfonts
+npm i yt-search
+npm i google-it
+npm i g-i-s
+npm i hxz-api
+npm i
+
+echo -e '\e[1;32m
+Descarga Completada al 100%
+Creador: Thechoute
+Numero: +18299897014
+Instagram: the_choute_
+Youtube: https://www.youtube.com/c/thechout
+Escribe node thechoute para iniciar el bot
+En caso que el bot presente algun problema, comunicate con el creador su numero esta mas arriba'

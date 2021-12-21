da121081aa95ff119cc4048585c3d4a8b33ff15b
thechoute <82709682+thechoute@users.noreply.github.com>
2021-12-21 11:46:53 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+clear
+apt-get update
+apt-get upgrade
+apt-get install nodejs -y
+apt-get install ffmpeg libwebp -y
+apt-get install wget -y
+apt-get install imagemagick -y
+apt-get install tesseract -y
+npm i node-tesseract-ocr
+npm i @adiwajshing/baileys@3.4.1
+pkg install termimage
+pkg install toilet
+pkg install cowsay
+pkg install neofetch
+neofetch
+npm i ffmpeg
+npm i cfonts
+npm i

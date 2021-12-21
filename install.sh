88a932b50c2f96c85287c2bd832fc1b0e4bcfc14
thechoute <82709682+thechoute@users.noreply.github.com>
2021-12-21 17:23:54 -0400

    📱

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/bash
-npm install
+apt-get install nodejs -y
+apt-get install ffmpeg libwebp -y
+apt-get install wget -y
+apt-get install imagemagick -y
+apt-get install tesseract -y
+npm i node-tesseract-ocr
+npm i @adiwajshing/baileys@3.4.1
+pkg install termimage
+pkg install toilet
+pkg install cowsay
+pkg install neofetch
 npm i ffmpeg
 npm i cfonts
+npm install
 npm i yt-search
 npm i google-it
 npm i g-i-s
 npm i hxz-api
-npm i node-fetch
-npm i moment-timezone
-npm i child_process
-npm i fluent-ffmpeg
-npm i 
+npm i
+
 
 echo -e '\e[1;32m
 Descarga Completada al 100%

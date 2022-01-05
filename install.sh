235673309d132fdaeb63aae01b507bdedc9e017a
thechoute <82709682+thechoute@users.noreply.github.com>
2022-01-05 00:33:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,17 @@ echo -e '\e[1;32m
 ░█▄─░█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀█ █▀▀█ ─▀─ █▀▀█ █▀▀ 
 ░█░█░█ █▀▀ █── █▀▀ ▀▀█ █▄▄█ █▄▄▀ ▀█▀ █──█ ▀▀█ 
 ░█──▀█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀──▀ ▀─▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀'
-apt-get install ffmpeg libwebp -y
-apt-get install wget -y
-apt-get install imagemagick -y
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 npm i qrcode
 

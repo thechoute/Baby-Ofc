f478c693621168cd30dbbd65790d692bef802bbe
thechoute <82709682+thechoute@users.noreply.github.com>
2022-01-08 06:55:38 -0400

    ✅

--- install.sh
+++ install.sh
@@ -17,28 +17,28 @@ apt-get install wget
 apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install -g npm@6
-npm install toml
-npm i crypto-js
-npm i moment-timezone
-npm i axios
-npm i node-fetch@2.6.1 - npm
-npm i requests 
-npm install request --save
-npm i cfonts
+npm install -g npm@6 -y 
+npm install toml -y
+npm i crypto-js -y
+npm i moment-timezone -y
+npm i axios -y
+npm i node-fetch@2.6.1 - npm -y
+npm i requests -y
+npm install request --save -y
+npm i cfonts -y
 npm i spinnies
-npm i image-to-base64
-npm i remove.bg
-npm i fluent-ffmpeg
-npm i @adiwajshing/baileys
-npm i performance-now
-npm i form-data
-npm i yt-search
-npm i google-it
-npm i g-i-s
-npm i hxz-api
-npm i cheerio
-npm i qrcode
+npm i image-to-base64 -y
+npm i remove.bg -y
+npm i fluent-ffmpeg -y
+npm i @adiwajshing/baileys -y
+npm i performance-now -y
+npm i form-data -y
+npm i yt-search -y
+npm i google-it -y
+npm i g-i-s -y
+npm i hxz-api -y
+npm i cheerio -y
+npm i qrcode -y
 
 echo -e '\e[1;32m
 Descarga Completada al 100%
